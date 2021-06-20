4000558090ad36c90f73b0047229e79759d95c17
firstripecherry <55585201+firstripecherry@users.noreply.github.com>
2021-06-20 16:08:50 +0300

    Add files via upload

--- 4.cpp
+++ 4.cpp
@@ -1,115 +1,31 @@
 ﻿#include <iostream>
+#include <queue>  
+
 using namespace std;
-template<typename T> 
-classQueue
-{
-public:
-	 Queue();
-	~Queue();
-	void delete_unit();
-	void push(T data);
-	void clear();
-	int GetSize() { return Size; } 
 
+int main() {
+	setlocale(LC_ALL, "rus");
+	queue <int> q;  
 
- T& operator[](const int index);
+	cout << "Пользователь, пожалуйста введите 5 чисел: " << endl;
 
-private:
-	template<typename T> 
-	class Node {
-	public:
-		Node* pNext; 
-		T data;
-		Node(T data = T(), Node* pNext = nullptr) { 
-			this->data = data;
-			this->pNext = pNext;
+	for (int h = 0; h < 5; h++) {
+		int a;
 
+		cin >> a;
 
-		}
-	};
-	int Size;
-	Node<T>* head;
-
-};
-template<typename T> 
-Queue<T>::Queue() { 
-	Size = 0;
-	head = nullptr;
-}
-template<typename T> 
-Queue<T>::~Queue() {
-	clear();
-}
-
-template<typename T>
-void Queue<T>::delete_unit() 
-{
-	Node<T>* temp = head;
-	head = head->pNext;
-	delete temp;
-	Size--;
-
-}
-
-template<typename T>
-void Queue<T>::push(T data)
-{
-	if (head == nullptr) {
-		head = new Node<T>(data);
-	}
-	else {
-		Node<T>* current = this->head;
-		while (current->pNext != nullptr) { 
-			current = current->pNext;
-		}
-		current->pNext = new Node<T>(data);
-
-	}
-	Size++;
-}
-
-template<typename T>
-void Queue<T>::clear()
-{
-	while (Size > 0) {
-		pop_front();
-	}
-}
-
-
-
-template<typename T>
-T& Queue<T>::operator[](const int index)
-{
-	int counter = 0;
-	Node<T>* current = this->head;
-	while (current != nullptr) {
-		if (counter == index) {
-			return current->data;
-		}
-		current = current->pNext;
-		counter++;
+		q.push(a);  
 	}
 
-}
+	cout << endl;
+	cout << "Самый первый элемент в очереди: " << q.front() << endl;  
+																	 
+	q.pop();  
 
+	cout << "Новый первый элемент (после удаления): " << q.front() << endl;
 
+	if (!q.empty()) cout << "Очередь не пуста!";  
 
-
-
-
-
-
-
-
-
-int main()
-{
-	Queue<int> q;
-	q.push(5);
-	q.push(10);
-	q.push(22);
-	q.delete_unit();
-	cout << q.operator[](0);
-	q.clear;
+	system("pause");
+	return 0;
 }
